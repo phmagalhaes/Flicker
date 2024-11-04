d3b732337b974827f58e6e574a2d720d40b4f68a
carolteixeir4 <carol.teixeira351@gmail.com>
2024-11-04 15:10:45 -0300

    perfil

--- lib/pages/confPerfil.dart
+++ lib/pages/confPerfil.dart
@@ -20,56 +20,69 @@ class UserProfileScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(color: MyColors.azulescuro),
-        padding: EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Meu perfil',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15), // Bordas arredondadas
-              child: Image.asset(
-                "assets/img/carol.jpg",
-                height: 110, // Ajuste a altura conforme desejado
-                width: 110,  // Ajuste a largura conforme desejado
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              'Carol',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 15),
+            const SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.edit, color: Colors.white, size: 18),
-                SizedBox(width: 5),
-                Text(
-                  'Editar perfil',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, top: 16.0),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
                 ),
               ],
             ),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Meu perfil',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        "assets/img/carol.jpg",
+                        height: 110,
+                        width: 110,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Carol',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.edit, color: Colors.white, size: 18),
+                        SizedBox(width: 5),
+                        Text(
+                          'Editar perfil',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             Expanded(
               child: Container(
                 padding: EdgeInsets.only(top: 16),
@@ -79,24 +92,26 @@ class UserProfileScreen extends StatelessWidget {
                     topRight: Radius.circular(20),
                   ),
                 ),
-                child: ListView.separated(
-                  itemCount: 5,
-                  separatorBuilder: (context, index) => Divider(
-                    color: const Color.fromARGB(
-                        255, 255, 255, 255), // Cor da linha
-                    thickness: 1,
+                child: Container(
+                  width: 337,
+                  child: ListView.separated(
+                    itemCount: 5,
+                    separatorBuilder: (context, index) => Divider(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      thickness: 1,
+                    ),
+                    itemBuilder: (context, index) {
+                      final titles = [
+                        'Minha Lista',
+                        'Configurações do Aplicativo',
+                        'Avisos Legais',
+                        'Ajuda',
+                        'Sair'
+                      ];
+                      return _buildListItem(
+                          titles[index], Icons.arrow_forward_ios, context);
+                    },
                   ),
-                  itemBuilder: (context, index) {
-                    final titles = [
-                      'Minha Lista',
-                      'Configurações do Aplicativo',
-                      'Avisos Legais',
-                      'Ajuda',
-                      'Sair'
-                    ];
-                    return _buildListItem(
-                        titles[index], Icons.arrow_forward_ios, context);
-                  },
                 ),
               ),
             ),
@@ -112,7 +127,7 @@ class UserProfileScreen extends StatelessWidget {
         title,
         style: TextStyle(
             color: Colors.white,
-            fontWeight: FontWeight.bold), // Texto em branco e negrito
+            fontWeight: FontWeight.bold),
       ),
       trailing: Icon(icon, color: Colors.white),
       onTap: () {
